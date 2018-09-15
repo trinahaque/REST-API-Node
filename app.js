63ba0e652b507541f2e36dcba4013aa05e0ca64c
trinahaque <trina.haque28@gmail.com>
2018-09-14 17:54:17 -0700

    email changed

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const app = express();
 
 app.use((requestAnimationFrame, res, next) => {
     res.status(200).json({
-        test: "second attempt"
+        message: "Email changed",
+        message2: "hello"
     });
 });
 
