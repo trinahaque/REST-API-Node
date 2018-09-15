f815b38ee22b2c803492440d290f0262ca798a69
trinahaque <trina_haque28@outlook.com>
2018-09-15 13:28:16 -0700

    error handling

--- app.js
+++ app.js
@@ -8,4 +8,22 @@ const orderRoutes = require('./api/routes/orders');
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+// handling errors when the route is different than the other two
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+// handling errors when the error occurs in other places (database)
+// basically all other errors
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
 module.exports = app;
